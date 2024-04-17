aeac547afa2ae2bbc9ec324d00e193b0544c96d0
RaymondBog <raymondbog@gmail.com>
2024-04-17 17:06:22 +0000

    further progress

--- src/Footer.js
+++ src/Footer.js
@@ -1,65 +1,98 @@
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faTwitter, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+
 const Footer = () => {
     return (
-    <div className="footer">
-    <div class="container my-5">
+        <div className="footer">
+            <div className="container my-5">
+                <footer style={{ background: 'rgba(37, 22, 111, 1)' }}>
+                    <div className="container p-4">
+                        <div className="row">
+                            <div className="col-lg-6 col-md-12 mb-4">
+                             
+                                  <h5 className="mb-5" style={{ letterSpacing: '2px', color: 'white' }}>Premier Nursing College</h5>
+                                <p style={{ color: 'white' }}>
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+                                    molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
+                                    voluptatem veniam, est atque cumque eum delectus sint!
+                                </p>
 
-        <footer style={{backgroundColor: '#deded5'}}>
-        <div className="container p-4">
-            <div className="row">
-            <div className="col-lg-6 col-md-12 mb-4">
-            <h5 className="mb-3" style={{letterSpacing: '2px',color: '#818963'}}>Premier Nursing College</h5>
-            <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-            molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
-            voluptatem veniam, est atque cumque eum delectus sint!
-            </p>
-            </div>
-            <div className="col-lg-3 col-md-6 mb-4">
-            <h5 className="mb-3" style={{letterSpacing: '2px',color: '#818963'}}>links</h5>
-            <ul className="list-unstyled mb-0">
-            <li className="mb-1">
-            <a href="#!" style={{color: '#4f4f4f'}}>Frequently Asked Questions</a>
-            </li>
-            <li className="mb-1">
-            <a href="#!" style={{color: '#4f4f4f'}}>Delivery</a>
-            </li>
-            <li className="mb-1">
-            <a href="#!" style={{color: '#4f4f4f'}}>Pricing</a>
-            </li>
-            <li>
-            <a href="#!" style={{color: '#4f4f4f'}}>Where we deliver?</a>
-            </li>
-            </ul>
-            </div>
-            <div className="col-lg-3 col-md-6 mb-4">
-            <h5 className="mb-1" style={{letterSpacing: '2px', color: '#818963'}}>opening hours</h5>
-            <table class="table" style={{color: '#4f4f4f', borderColor: '#666'}}>
-            <tbody>
-            <tr>
-                <td>Mon - Fri:</td>
-                <td>8am - 9pm</td>
-            </tr>
-            <tr>
-                <td>Sat - Sun:</td>
-                <td>8am - 1am</td>
-            </tr>
-            </tbody>
-            </table>
-            </div>
+                                <p className='mb-4' style={{ color: 'white' }}>+2338096542356 <br />
+                                    info@premeirnursingcollege.com
+                                </p>
+
+                            
+
+                            </div>
+                            <div className="col-lg-3 col-md-6 mb-4 d-flex">
+                                <div>
+                                <h5 className="mb-3" style={{ letterSpacing: '2px', color: 'white'}}>Quick links</h5>
+                                <ul className="list-unstyled mb-0">
+                                    <li className="mb-1">
+                                        <a href="#!" style={{ color: 'white',textDecoration:'none' }}>Programs</a>
+                                    </li>
+                                    <li className="mb-1">
+                                        <a href="#!" style={{ color: 'white',textDecoration:'none' }}>Admissions</a>
+                                    </li>
+                                    <li className="mb-1">
+                                        <a href="#!" style={{ color: 'white',textDecoration:'none' }}>Faculty</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" style={{ color: 'white',textDecoration:'none' }}>Staff</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" style={{ color: 'white',textDecoration:'none' }}>Student Portal</a>
+                                    </li>
+                                </ul>
+                                </div>
+                                <div >
+                                <ul className="mb-0 " style={{listStyleType:'none'}}>
+                                    <li className="mb-1" style={{marginTop: '60px'}}>
+                                        <a href="#!" style={{ color: 'white',textDecoration:'none' }}>Programs</a>
+                                    </li>
+                                    <li className="mb-1">
+                                        <a href="#!" style={{ color: 'white',textDecoration:'none' }}>Admissions</a>
+                                    </li>
+                                    <li className="mb-1">
+                                        <a href="#!" style={{ color: 'white',textDecoration:'none' }}>Faculty</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" style={{ color: 'white',textDecoration:'none' }}>Staff</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" style={{ color: 'white',textDecoration:'none' }}>Student Portal</a>
+                                    </li>
+                                </ul>
+                                </div>
+                            </div>
+                            <div className="col-lg-3 col-md-6 mb-4">
+                                <div>
+                                    <a href="#!" style={{ color: 'white', marginRight: '10px' }}>
+                                        <FontAwesomeIcon icon={faFacebookF} />
+                                    </a>
+                                    <a href="#!" style={{ color: 'white', marginRight: '10px' }}>
+                                        <FontAwesomeIcon icon={faInstagram} />
+                                    </a>
+                                    <a href="#!" style={{ color: 'white', marginRight: '10px' }}>
+                                        <FontAwesomeIcon icon={faTwitter} />
+                                    </a>
+                                    <a href="#!" style={{ color: 'white', marginRight: '10px' }}>
+                                        <FontAwesomeIcon icon={faLinkedinIn} />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)', color: 'white' }}>
+                        <p>© Premier Nursing College 2023</p>
+                    </div>
+                </footer>
             </div>
         </div>
-        <div className="text-center p-3" style={{backgroundColor: 'rgba(0, 0, 0, 0.2)'}}>
-        © 2020 Copyright:
-        <a className="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-        </div>
-
-        </footer>
-
-</div>
-    </div>
     );
-  }
-  
-  export default Footer;
+}
+
+export default Footer;
+
   
