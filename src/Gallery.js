246e416ccf3554b747f1432dad402e7a90aae345
RaymondBog <raymondbog@gmail.com>
2024-05-13 17:50:49 +0000

    deployment changes

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import gallery1 from './assets/images/gallery1.jpeg';
 import gallery2 from './assets/images/gallery2.jpeg';
